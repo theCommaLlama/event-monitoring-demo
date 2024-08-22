1c98a1ae5e6ad310ad86c7e91ad494d725293112
theCommaLlama <135273806+theCommaLlama@users.noreply.github.com>
2024-08-21 21:31:41 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,18 @@
 #sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --userprefix event-monitoring -o work.shop
 #sfdx shane:tsp:username:update
-sfdx force:package:install -p 04t1t000003Po3QAAS -w 10
-sfdx force:source:push
-sfdx force:user:permset:assign -n Event_Monitoring_Access
+
+#sfdx force:package:install -p 04t1t000003Po3QAAS -w 10
+sf package install -p 04t1t000003Po3QAAS -w 10
+
+#sfdx force:source:push
+sf project deploy start
+
+#sfdx force:user:permset:assign -n Event_Monitoring_Access
+sf org assign permset -n Event_Monitoring_Access
+
 #sfdx shane:user:password:set -p salesforce1 -g User -l User
+
 sf org create user -a other-user
-sfdx force:org:open
+
+#sfdx force:org:open
+sf org open
